61baed900372359d9414267fdd055e204774b71f
Endris M <EndrisMd@gmail.com>
2023-05-04 23:14:50 +0300

    In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
---A SQL script that creates a table with unique users.
+-- A SQL script that creates a table with unique users.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
